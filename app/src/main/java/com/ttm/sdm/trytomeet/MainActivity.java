885dbd8fde6f480e921ca5af8c9175dec7ed33f4
VictorIranzo <viranzo96@gmail.com>
2018-03-12 16:15:55 +0100

    Primeros pasos en la creación de la barra de navegación
    Se han usado elementos fake para el menú.

--- app/src/main/java/com/ttm/sdm/trytomeet/MainActivity.java
+++ app/src/main/java/com/ttm/sdm/trytomeet/MainActivity.java
@@ -1,13 +1,74 @@
 package com.ttm.sdm.trytomeet;
 
+import android.content.res.Configuration;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity
+        extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener
+{
+
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Shows the home button in the action bar.
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        // Creates the toggle associated to the drawer.
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        toggle = new ActionBarDrawerToggle(this, drawerLayout,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+
+        NavigationView navigationView =
+                (NavigationView) findViewById(R.id.navView);
+
+        // Associates the toogle to receive events from the drawer.
+        navigationView.setNavigationItemSelectedListener(this);
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        // Synchronize the toggle with the drawer state.
+        toggle.syncState();
+    }
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        // Synchronize the toggle with the drawer state.
+        toggle.syncState();
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (toggle.onOptionsItemSelected(item)) {
+            // TODO: Review if here must be added more code.
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        drawerLayout.closeDrawers();
+        return true;
     }
 }
