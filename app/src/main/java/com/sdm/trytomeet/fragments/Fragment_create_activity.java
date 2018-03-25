81bd1da005a86dcb003b639cdf45b0206da49842
adrymc96 <admarcar@inf.upv.es>
2018-03-25 13:43:46 +0200

    Ventana crear evento

---
+++ app/src/main/java/com/sdm/trytomeet/fragments/Fragment_create_activity.java
@@ -0,0 +1,29 @@
+package com.sdm.trytomeet.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sdm.trytomeet.R;
+
+public class Fragment_create_activity extends Fragment {
+
+    public Fragment_create_activity() {
+        // Required empty public constructor
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_create_activity, container, false);
+    }
+}
